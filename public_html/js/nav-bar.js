28b90b3e5a8ee6e3d8d99dfd867f7a929b4db4d2
Eric Lefort <leforte@mcmaster.ca>
2019-04-22 01:45:09 -0400

    Fixed horizontal scaling on contact, resume and projects pages

--- public_html/js/nav-bar.js
+++ public_html/js/nav-bar.js
@@ -4,14 +4,14 @@ $(document).scroll(function(){
 
 	if(top > convertRemToPixels(7)){
 		$(".header .title").remove();
-		$(".header .top-nav-links").css("top", "26%")
+		$(".header .top-nav-links").css("top", "0")
 		$(".header").css("position", "fixed");
-		$(".header").css("min-height", "3.7rem");
+		$(".header").css("min-height", "5rem");
 	}else{
 		$(".header").css("position", "absolute");
 		$(".header").css("min-height", "10rem");
-		$(".header .top-nav-links").css("top", "73%")
-		$(".header").prepend("<h1 class=\"title\">Projects</h1>");
+		$(".header .top-nav-links").css("top", "5rem")
+		$(".header").prepend("<h1 class=\"title\">Welcome</h1>");
 	}
 });
 
