d4fd602b19e4b37c59b783addfee8c2265eb2500
Eric Lefort <leforte@mcmaster.ca>
2019-04-22 00:17:47 -0400

    Fixed text scaling on homepage

--- public_html/js/scroll-actions.js
+++ public_html/js/scroll-actions.js
@@ -49,13 +49,13 @@ $(document).scroll(function(){
 
 	if(top > convertRemToPixels(7)){
 		$(".header .title").remove();
-		$(".header .top-nav-links").css("top", "26%")
+		$(".header .top-nav-links").css("top", "0")
 		$(".header").css("position", "fixed");
-		$(".header").css("min-height", "3.7rem");
+		$(".header").css("min-height", "5rem");
 	}else{
 		$(".header").css("position", "absolute");
 		$(".header").css("min-height", "10rem");
-		$(".header .top-nav-links").css("top", "73%")
+		$(".header .top-nav-links").css("top", "5rem")
 		$(".header").prepend("<h1 class=\"title\">Welcome</h1>");
 	}
 
